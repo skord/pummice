775aefc4d5c31d726b448ce49168bef624fde725
Mike Danko <mike@l4m3.com>
2012-11-20 20:47:44 -0500

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pummice::Application.config.secret_token = '07c0fbbe8f6fcbafb89bc4fe8456947ac0f2f890a9758b113dd0b4f7ca7f8546e3433743510808588549de42f48cb955d500fcab9e54b1fab8daaa10832f4d15'
