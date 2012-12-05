e307f37de6b1b80ee54a66d98683c05c0fef450f
Steve Ogdahl <steve.ogdahl@gmail.com>
2012-12-04 23:53:56 -0600

    Fixing spec "validate" methods for game and user models Now, currently, 'rake spec' is not failing any more!

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,10 @@
 class Game < ActiveRecord::Base
   attr_accessible :name
   has_and_belongs_to_many :users
+
+  validate :correct_number_of_users
+
+  def correct_number_of_users
+    errors.add(:base, "Incorrect number of users") if users.count > 4
+  end
 end
