e0b70c99b63dbeaa341276c6c55ceda5f114c43b
Steve Ogdahl <steve.ogdahl@gmail.com>
2012-12-18 22:01:13 -0600

    Adding 3 new settings to the Game model:   * variant -- France or Ireland   * is_short_game -- specifying a short game (instead of the normal length)   * use_loamy_landscape -- specifies that the Loamy Landscape card should be used in the Ireland variant (promo card)

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,5 @@
 class Game < ActiveRecord::Base
-  attr_accessible :name
+  attr_accessible :name, :variant, :is_short_game, :use_loamy_landscape
   has_and_belongs_to_many :users
 
   validate :correct_number_of_users
@@ -7,4 +7,9 @@ class Game < ActiveRecord::Base
   def correct_number_of_users
     errors.add(:base, "Incorrect number of users") if users.count > 4
   end
+end
+
+class GameVariant
+  FRANCE = 0
+  IRELAND = 1
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121202205931) do
+ActiveRecord::Schema.define(:version => 20121219011744) do
 
   create_table "games", :force => true do |t|
     t.string   "name"
-    t.datetime "created_at",                   :null => false
-    t.datetime "updated_at",                   :null => false
-    t.boolean  "active",     :default => true
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+    t.boolean  "active",              :default => true
+    t.integer  "variant",             :default => 0
+    t.boolean  "is_short_game",       :default => false
+    t.boolean  "use_loamy_landscape", :default => false
   end
 
   create_table "games_users", :id => false, :force => true do |t|
@@ -28,8 +31,8 @@ ActiveRecord::Schema.define(:version => 20121202205931) do
   add_index "games_users", ["game_id", "user_id"], :name => "index_games_users_on_game_id_and_user_id"
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "",                           :null => false
-    t.string   "encrypted_password",     :default => "",                           :null => false
+    t.string   "email",                  :default => "",                                        :null => false
+    t.string   "encrypted_password",     :default => "",                                        :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -38,12 +41,12 @@ ActiveRecord::Schema.define(:version => 20121202205931) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                                                       :null => false
-    t.datetime "updated_at",                                                       :null => false
+    t.datetime "created_at",                                                                    :null => false
+    t.datetime "updated_at",                                                                    :null => false
     t.string   "lastname"
     t.string   "firstname"
     t.boolean  "admin"
-    t.string   "timezone",               :default => "Central Time (US & Canada)"
+    t.string   "timezone",               :default => "Central Time (US & Canada), (GMT-06:00)"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
